b18f56cf32297aa414031a6e9baccc9615fe2187
Thibault Martinez <thibault@iota.org>
2020-01-07 09:21:04 +0100

    Rustfmt

--- iota-ss/ed25519.rs
+++ iota-ss/ed25519.rs
@@ -1,19 +1,17 @@
-extern crate rand;
 extern crate ed25519_dalek;
+extern crate rand;
 
-use std::marker::PhantomData;
-use iota_crypto::{HashMode, Sponge, subseed};
-use rand::rngs::OsRng;
+use super::*;
 use ed25519_dalek::Keypair;
 use ed25519_dalek::Signature;
-use super::*;
+use iota_crypto::{subseed, HashMode, Sponge};
+use rand::rngs::OsRng;
+use std::marker::PhantomData;
 
 #[derive(Default)]
-pub struct Ed25519PrivateKeyGeneratorBuilder {
-}
+pub struct Ed25519PrivateKeyGeneratorBuilder {}
 
-pub struct Ed25519PrivateKeyGenerator {
-}
+pub struct Ed25519PrivateKeyGenerator {}
 
 pub struct Ed25519PrivateKey {
     keypair: ed25519_dalek::Keypair,
@@ -29,8 +27,7 @@ pub struct Ed25519Signature {
 
 impl Ed25519PrivateKeyGeneratorBuilder {
     pub fn build(&mut self) -> Ed25519PrivateKeyGenerator {
-        Ed25519PrivateKeyGenerator {
-        }
+        Ed25519PrivateKeyGenerator {}
     }
 }
 
@@ -38,12 +35,10 @@ impl crate::PrivateKeyGenerator for Ed25519PrivateKeyGenerator {
     type PrivateKey = Ed25519PrivateKey;
 
     fn generate(&self, seed: &[i8], index: usize) -> Self::PrivateKey {
-        let mut csprng = OsRng{};
+        let mut csprng = OsRng {};
         let keypair: Keypair = Keypair::generate(&mut csprng);
 
-        Ed25519PrivateKey{
-            keypair: keypair,
-        }
+        Ed25519PrivateKey { keypair: keypair }
     }
 }
 
@@ -52,18 +47,17 @@ impl crate::PrivateKey for Ed25519PrivateKey {
     type Signature = Ed25519Signature;
 
     fn generate_public_key(&self) -> Self::PublicKey {
-
-        Ed25519PublicKey{
+        Ed25519PublicKey {
             key: self.keypair.public,
         }
     }
 
     // TODO: hash ? enforce size ?
     fn sign(&mut self, message: &[i8]) -> Self::Signature {
-        let test = unsafe { &*(message as *const _  as *const [u8]) };
+        let test = unsafe { &*(message as *const _ as *const [u8]) };
         let signature = self.keypair.sign(test);
 
-        Ed25519Signature{
+        Ed25519Signature {
             signature: signature,
         }
     }
@@ -73,7 +67,7 @@ impl crate::PublicKey for Ed25519PublicKey {
     type Signature = Ed25519Signature;
 
     fn verify(&self, message: &[i8], signature: &Self::Signature) -> bool {
-        let test = unsafe { &*(message as *const _  as *const [u8]) };
+        let test = unsafe { &*(message as *const _ as *const [u8]) };
 
         self.key.verify(test, &signature.signature).is_ok()
     }
@@ -117,19 +111,18 @@ impl crate::Signature for Ed25519Signature {
 #[cfg(test)]
 mod tests {
 
-    use iota_conversion::Trinary;
     use super::*;
+    use iota_conversion::Trinary;
     const SEED1: &str =
         "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN";
     const SEED2: &str =
         "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNDNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN";
 
     fn ed25519_generic_test() {
-
         let seed_trits_1 = &SEED1.trits();
         let seed_trits_2 = &SEED2.trits();
 
-        for index in 0..25  {
+        for index in 0..25 {
             let private_key_generator = Ed25519PrivateKeyGeneratorBuilder::default().build();
             // TODO mut ?
             let mut private_key = private_key_generator.generate(&seed_trits_1, index);
@@ -140,7 +133,6 @@ mod tests {
             assert!(valid);
             valid = public_key.verify(seed_trits_2, &signature_good);
             assert!(!valid);
-
         }
     }
 
--- iota-ss/wots_v1.rs
+++ iota-ss/wots_v1.rs
@@ -1,35 +1,35 @@
 // TODO clean use
-use std::marker::PhantomData;
-use iota_crypto::{HashMode, Sponge, subseed};
 use super::*;
+use iota_crypto::{subseed, HashMode, Sponge};
+use std::marker::PhantomData;
 
 // TODO state as Vec<i8> ?
 
 #[derive(Default)]
 pub struct WotsV1PrivateKeyGeneratorBuilder<S> {
     security_level: Option<u8>,
-    _sponge: PhantomData<S>
+    _sponge: PhantomData<S>,
 }
 
 #[derive(Default, Debug)]
 pub struct WotsV1PrivateKeyGenerator<S> {
     security_level: u8,
-    _sponge: PhantomData<S>
+    _sponge: PhantomData<S>,
 }
 
 pub struct WotsV1PrivateKey<S> {
     state: Vec<i8>,
-    _sponge: PhantomData<S>
+    _sponge: PhantomData<S>,
 }
 
 pub struct WotsV1PublicKey<S> {
     state: Vec<i8>,
-    _sponge: PhantomData<S>
+    _sponge: PhantomData<S>,
 }
 
 pub struct WotsV1Signature<S> {
     state: Vec<i8>,
-    _sponge: PhantomData<S>
+    _sponge: PhantomData<S>,
 }
 
 impl<S: Sponge> WotsV1PrivateKeyGeneratorBuilder<S> {
@@ -40,16 +40,14 @@ impl<S: Sponge> WotsV1PrivateKeyGeneratorBuilder<S> {
 
     pub fn build(&mut self) -> Result<WotsV1PrivateKeyGenerator<S>, String> {
         match self.security_level {
-            Some(security_level) =>
-                match security_level {
-                    1 | 2 | 3 =>
-                        Ok(WotsV1PrivateKeyGenerator {
-                            security_level: security_level,
-                            _sponge: PhantomData
-                        }),
-                    _ => Err("Invalid security level, possible values are 1, 2 or 3".to_string())
-                },
-            None => Err("Security level has not been set".to_string())
+            Some(security_level) => match security_level {
+                1 | 2 | 3 => Ok(WotsV1PrivateKeyGenerator {
+                    security_level: security_level,
+                    _sponge: PhantomData,
+                }),
+                _ => Err("Invalid security level, possible values are 1, 2 or 3".to_string()),
+            },
+            None => Err("Security level has not been set".to_string()),
         }
     }
 }
@@ -74,9 +72,9 @@ impl<S: Sponge> crate::PrivateKeyGenerator for WotsV1PrivateKeyGenerator<S> {
 
         sponge.reset();
 
-        WotsV1PrivateKey{
+        WotsV1PrivateKey {
             state: state,
-            _sponge: PhantomData
+            _sponge: PhantomData,
         }
     }
 }
@@ -92,7 +90,7 @@ impl<S: Sponge> crate::PrivateKey for WotsV1PrivateKey<S> {
         let mut digests = Vec::new();
 
         for chunk in hashed_private_key.chunks_mut(243) {
-            for _ in 0..26  {
+            for _ in 0..26 {
                 sponge.absorb(chunk).unwrap();
                 sponge.squeeze(chunk).unwrap();
                 sponge.reset();
@@ -110,9 +108,9 @@ impl<S: Sponge> crate::PrivateKey for WotsV1PrivateKey<S> {
         sponge.squeeze(&mut hash).unwrap();
         sponge.reset();
 
-        WotsV1PublicKey{
+        WotsV1PublicKey {
             state: hash.to_vec(),
-            _sponge: PhantomData
+            _sponge: PhantomData,
         }
     }
 
@@ -124,16 +122,16 @@ impl<S: Sponge> crate::PrivateKey for WotsV1PrivateKey<S> {
         for (i, chunk) in signature.chunks_mut(243).enumerate() {
             let val = message[i * 3] + message[i * 3 + 1] * 3 + message[i * 3 + 2] * 9;
 
-            for _ in 0..(13 - val)  {
+            for _ in 0..(13 - val) {
                 sponge.absorb(chunk).unwrap();
                 sponge.squeeze(chunk).unwrap();
                 sponge.reset();
             }
         }
 
-        WotsV1Signature{
+        WotsV1Signature {
             state: signature,
-            _sponge: PhantomData
+            _sponge: PhantomData,
         }
     }
 }
@@ -154,7 +152,7 @@ impl<S: Sponge> crate::PublicKey for WotsV1PublicKey<S> {
 
 impl<S: Sponge> WotsV1Signature<S> {
     pub fn new(state: &[i8]) -> WotsV1Signature<S> {
-        WotsV1Signature{
+        WotsV1Signature {
             state: state.to_vec(),
             _sponge: PhantomData,
         }
@@ -183,7 +181,7 @@ impl<S: Sponge> crate::RecoverableSignature for WotsV1Signature<S> {
         for (i, chunk) in state.chunks_mut(243).enumerate() {
             let val = message[i * 3] + message[i * 3 + 1] * 3 + message[i * 3 + 2] * 9;
 
-            for _ in 0..(val - -13)  {
+            for _ in 0..(val - -13) {
                 sponge.absorb(chunk).unwrap();
                 sponge.squeeze(chunk).unwrap();
                 sponge.reset();
@@ -201,9 +199,9 @@ impl<S: Sponge> crate::RecoverableSignature for WotsV1Signature<S> {
         sponge.squeeze(&mut hash).unwrap();
         sponge.reset();
 
-        WotsV1PublicKey{
+        WotsV1PublicKey {
             state: hash.to_vec(),
-            _sponge: PhantomData
+            _sponge: PhantomData,
         }
     }
 }
@@ -211,27 +209,32 @@ impl<S: Sponge> crate::RecoverableSignature for WotsV1Signature<S> {
 #[cfg(test)]
 mod tests {
 
-    use iota_crypto::{Kerl};
-    use iota_conversion::Trinary;
     use super::*;
+    use iota_conversion::Trinary;
+    use iota_crypto::Kerl;
     const SEED: &str =
         "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN";
 
     fn wots_v1_generic_test<S: Sponge>() {
-
         let seed_trits = &SEED.trits();
 
         for security in 1..4 {
-            for index in 0..25  {
-                let private_key_generator = WotsV1PrivateKeyGeneratorBuilder::<S>::default().security_level(security).build().unwrap();
+            for index in 0..25 {
+                let private_key_generator = WotsV1PrivateKeyGeneratorBuilder::<S>::default()
+                    .security_level(security)
+                    .build()
+                    .unwrap();
                 // TODO mut ?
                 let mut private_key = private_key_generator.generate(&seed_trits, index);
                 let public_key = private_key.generate_public_key();
-                let bytes =  public_key.to_bytes();
+                let bytes = public_key.to_bytes();
                 println!("{:?}", public_key.to_bytes().trytes());
                 let signature = private_key.sign(seed_trits);
                 let recovered_public_key = signature.recover_public_key(seed_trits);
-                assert!(all_equal(public_key.to_bytes(), recovered_public_key.to_bytes()));
+                assert!(all_equal(
+                    public_key.to_bytes(),
+                    recovered_public_key.to_bytes()
+                ));
                 let valid = public_key.verify(seed_trits, &signature);
                 assert!(valid);
             }
@@ -240,20 +243,55 @@ mod tests {
 
     #[test]
     fn wots_v1_generator_missing_security_level_test() {
-        assert_eq!(WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default().build().unwrap_err(), "Security level has not been set");
+        assert_eq!(
+            WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default()
+                .build()
+                .unwrap_err(),
+            "Security level has not been set"
+        );
     }
 
     #[test]
     fn wots_v1_generator_invalid_security_level_test() {
-        assert_eq!(WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default().security_level(0).build().unwrap_err(), "Invalid security level, possible values are 1, 2 or 3");
-        assert_eq!(WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default().security_level(4).build().unwrap_err(), "Invalid security level, possible values are 1, 2 or 3");
+        assert_eq!(
+            WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default()
+                .security_level(0)
+                .build()
+                .unwrap_err(),
+            "Invalid security level, possible values are 1, 2 or 3"
+        );
+        assert_eq!(
+            WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default()
+                .security_level(4)
+                .build()
+                .unwrap_err(),
+            "Invalid security level, possible values are 1, 2 or 3"
+        );
     }
 
     #[test]
     fn wots_v1_generator_valid_test() {
-        assert_eq!(WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default().security_level(1).build().is_ok(), true);
-        assert_eq!(WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default().security_level(2).build().is_ok(), true);
-        assert_eq!(WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default().security_level(3).build().is_ok(), true);
+        assert_eq!(
+            WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default()
+                .security_level(1)
+                .build()
+                .is_ok(),
+            true
+        );
+        assert_eq!(
+            WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default()
+                .security_level(2)
+                .build()
+                .is_ok(),
+            true
+        );
+        assert_eq!(
+            WotsV1PrivateKeyGeneratorBuilder::<Kerl>::default()
+                .security_level(3)
+                .build()
+                .is_ok(),
+            true
+        );
     }
 
     // #[test]
